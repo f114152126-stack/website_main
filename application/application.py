d16642591c17238d31edf32ca2328a60e4c3bf25
邵榆翔 <f114152126@nkust.edu.tw>
2026-06-06 19:37:04 +0800

    Change rendered template from index.html to home.html

--- application/application.py
+++ application/application.py
@@ -31,7 +31,7 @@ def loading():
 def home():
     """Renders the 'Home' page of the website."""
 
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 @app.route("/about")
